05725083cea8e0b2a2fd3cf05c842f916414792d
xoscownaks <doo9810@gmail.com>
2017-05-17 09:30:52 +0900

    상단 슬라이드 부분 구현 중

--- www/js/app.js
+++ www/js/app.js
@@ -3,18 +3,51 @@
 /****************************************
  사이드 메뉴 클릭 정의 
  * *************************************/
-window.fn = {};
-
-window.fn.open = function() {
-  var menu = document.getElementById('menu');
-  menu.open();
-};
-
-window.fn.load = function(page) {
-  var content = document.getElementById('content');
-  var menu = document.getElementById('menu');
-  content.load(page)
-    .then(menu.close.bind(menu));
-};
+    window.fn = {};
+    
+    window.fn.open = function() {
+      var menu = document.getElementById('menu');
+      menu.open();
+    };
+    
+    window.fn.load = function(page) {
+      var content = document.getElementById('content');
+      var menu = document.getElementById('menu');
+      content.load(page)
+        .then(menu.close.bind(menu));
+    };
 
 /***************************************/
+
+/******************************************
+ * 상단 슬라이드 메뉴 부분 
+ * ****************************************/
+    ons.ready(function() {
+      var carousel = document.addEventListener('postchange', function(event) {
+        console.log('Changed to ' + event.activeIndex)
+      });
+    });
+    
+    var next = function(){
+        var carousel = document.getElementById('carousel');
+        carousel.next();
+    };
+    
+    
+    
+    var myIndex = 0;
+    carousel();
+    
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        for (i = 0; i < x.length; i++) {
+           x[i].style.display = "none";  
+        }
+        myIndex++;
+        if (myIndex > x.length) {myIndex = 1}    
+        x[myIndex-1].style.display = "block";  
+        setTimeout(carousel, 2000); // Change image every 2 seconds
+    }
+
+/*************************************************/
